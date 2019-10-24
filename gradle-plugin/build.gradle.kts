2bccb61f4a38866d0deabdd87e3149a884cace67
Kenzie Togami <ket1999@gmail.com>
2019-10-24 12:19:53 -0700

    Ensure gradle publishes last

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -50,6 +50,11 @@ gradlePlugin {
 }
 
 
+tasks.named("publishPlugins").configure {
+    // The nexus tasks are trickier -- don't publish to Gradle until they are known-good
+    mustRunAfter(":cli:publish")
+    mustRunAfter(":common:publish")
+}
 rootProject.tasks.named("afterReleaseBuild").configure {
     dependsOn(tasks.named("publishPlugins"))
 }
